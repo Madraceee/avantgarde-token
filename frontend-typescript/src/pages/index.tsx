fc88892c3f6e2615ef6c9e7ae43263bca4f6ab8e
madraceee <raajanitheesh@gmail.com>
2023-02-08 13:02:58 +0530

    typescript files deleted

--- frontend-typescript/src/pages/index.tsx
+++ frontend-typescript/src/pages/index.tsx
@@ -13,7 +13,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Create Next App</title>
+        <title>Avantgarde Token</title>
         <meta name="description" content="AvantGarde Token" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
